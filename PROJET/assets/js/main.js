49821e7643f43837a2f048ebcc89f6d9be53d343
1er-RIMa <Projet-1erRIMa@outlook.fr>
2026-03-18 11:55:03 +0100

    Delete PROJET directory

--- PROJET/assets/js/main.js
+++
@@ -1,6 +0,0 @@
-// sert a afficher ou masquer la mission 
-document.getElementById('show-mission').addEventListener('click', function () {
-    const mission = document.getElementById('mission');
-    mission.classList.toggle('hidden');
-    this.textContent = mission.classList.contains('hidden') ? 'Voir la mission' : 'Masquer';
-});
